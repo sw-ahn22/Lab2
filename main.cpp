e675574a10dbef3af40aacb9c31ff3f806018dcf
sw-ahn22 <102525985+sw-ahn22@users.noreply.github.com>
2023-06-14 09:58:40 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
-vector<int> vec;
+vector< vector<int> > combination(int N, int M, int i){
+   vector< vector<int> > result;
 
-void func(int cnt) {
+   for (auto elem : combination(N, M-1, i+1)){
+      elem.push_back(i);
+   }
+   combination(N, M, i+1);
 
-
-
-
-
-
-
+   return result;
 }
 
+
 int main() {
-   cin >> N >> M;
-   func(0);
+   int N, M;
+   cin >> N;
+   cin >> M;
+
+   for (auto elem : combination(N, M, 1)){
+      for (int i = elem.size()-1; i>=0; i--){
+         cout << elem[i] << " ";
+      }
+      cout << endl;
+   }
+   
+
+   return 0;
 }
